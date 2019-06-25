eab7ff7a267d02596928f15d5f85f79d1ec294ac
ch1pset <theslenderpsycho@gmail.com>
2019-06-25 00:23:17 -0500

    Removed errors

--- ts_src/main.ts
+++ ts_src/main.ts
@@ -5,10 +5,10 @@ import { SpeedrunModule } from './bot/modules/speedrun-module.js';
 
 const bot = new BotClient(auth.token.test);
 const ping = new PingModule();
-const speedrun = new SpeedrunModule();
+// const speedrun = new SpeedrunModule();
 
 ping.subscribe(bot, 'ping');
 ping.subscribe(bot, 'pingme');
-speedrun.subscribe(bot, 'wr');
-speedrun.subscribe(bot, 'top');
-speedrun.subscribe(bot, 'place');
+// speedrun.subscribe(bot, 'wr');
+// speedrun.subscribe(bot, 'top');
+// speedrun.subscribe(bot, 'place');
--- ts_src/speedrun/api.ts
+++ ts_src/speedrun/api.ts
@@ -9,7 +9,7 @@ function GET(request: string | IRequest | URL, callback: (err: Error, data: stri
 {
     const wstream = new WSStream();
     https.get(request, response => response.pipe(wstream));
-    wstream.on('finish', () => callback(undefined, wstream.data));
+    wstream.on('finish', () => callback(undefined, JSON.parse(wstream.data).data));
     wstream.on('error', err => callback(err, undefined));
     return wstream;
 }
@@ -17,31 +17,35 @@ function GET(request: string | IRequest | URL, callback: (err: Error, data: stri
 function getGame(abrv, embeds)
 {
     const srcReq = createRequest(`/games?abbreviation=${abrv}&embed=${embeds.join(',')}`);
-    return GET(srcReq).then(games => new Game(games[0]));
+    GET(srcReq, (err, games) => {
+        const game = new Game(games[0]);
+    });
 }
 
 function getUser(name)
 {
     const srcReq = createRequest(`/users?lookup=${name}`);
-    return GET(srcReq).then(users => users[0]);
-}
-
-export async function getLeader(abrv: string, cat: string, vars: string[][], lvl: string)
-{
-    let game = await getGame(abrv, ['levels.variables', 'categories.variables']);
-    let category = game.getCategory(cat);
-    let level = game.getLevel(lvl);
-    let query = "";
-
-    if(vars) vars.forEach(v => {
-        let catvars = category.getVariableIDs(v[0], v[1]);
-        if(catvars) {
-            query += `var-${catvars[0]}=${catvars[1]}&`;
-        }
-        // let lvlvars = level.getVariableIDs(v[0], v[1]);
-        // if(lvlvars) {
-        //     query += `var-${lvlvars[0]}=${lvlvars[1]}&`;
-        // }
+    return GET(srcReq, (err, users) => {
+        const user = users[0];
     });
-    return GET(createRequest(`/leaderboards/${game.id}${level? `/level/${level.id}/` : `/category/`}${category.id}?${query}`));
 }
+
+// export async function getLeader(abrv: string, cat: string, vars: string[][], lvl: string)
+// {
+//     let game = await getGame(abrv, ['levels.variables', 'categories.variables']);
+//     let category = game.getCategory(cat);
+//     let level = game.getLevel(lvl);
+//     let query = "";
+
+//     if(vars) vars.forEach(v => {
+//         let catvars = category.getVariableIDs(v[0], v[1]);
+//         if(catvars) {
+//             query += `var-${catvars[0]}=${catvars[1]}&`;
+//         }
+//         // let lvlvars = level.getVariableIDs(v[0], v[1]);
+//         // if(lvlvars) {
+//         //     query += `var-${lvlvars[0]}=${lvlvars[1]}&`;
+//         // }
+//     });
+//     return GET(createRequest(`/leaderboards/${game.id}${level? `/level/${level.id}/` : `/category/`}${category.id}?${query}`));
+// }
