df6c07e2147b9b188da75d530fa3c31f9abdcd99
ch1pset <theslenderpsycho@gmail.com>
2019-06-27 00:53:43 -0500

    injector decorator now only accepts Service-likes

--- ts_src/utils/injector.ts
+++ ts_src/utils/injector.ts
@@ -1,24 +1,69 @@
+import { fn, Constructor, Singleton, Decorator } from "./types";
 
-const _PROVIDERS = {};
+// export class Injector {
+//     private _providers: Service[] = [];
+    
+//     private static _self: Injector = new Injector();
+//     private constructor() {
+//         console.log(`Injector was instantiated`);
+//     }
+//     static get self() { return Injector._self; }
 
-function getProvider(name: string) {
-    return _PROVIDERS[name];
+//     newProvider(ctor: Singleton<Service>): void {
+//         // console.log(ctor.prototype, ctor.self);
+//         this._providers.push(ctor.self);
+//     }
+//     getProvider(ctor: Singleton<Service>): Service {
+//         return this._providers.find(p => p === ctor.self);
+//     }
+// }
+
+// export function Provider(ctor: Singleton<Service>) {
+//     Injector.self.newProvider(ctor);
+//     // console.log(`Registered Provider: `, Injector.self.getProvider(ctor));
+// }
+
+// export function Inject(providers: Singleton<Service>[]) {
+//     return function<T extends {new(...args:any[]):{}}>(constructor:T) {
+//         return class extends constructor {
+//             constructor(...args: any[]) {
+//                 super(args);
+//             }
+//         }
+//     }
+// }
+
+export abstract class Service {
+    private static readonly _self: Service;
+    private constructor() { }
+    static self: Service;
 }
 
-class Injector {
-    private _providers: {[name: string]: Object | Function}
-}
-
-export function Provider(ctor: {new (...args: any[])}) {
-    _PROVIDERS[ctor.name] = new ctor();
-    console.log(`Registered Provider: `, getProvider(ctor.name));
-}
-
-export function Inject(providers: Function[]) {
-    return function(ctor: {new(...args: any[])}) {
-        providers.forEach(provider => {
-            ctor.prototype[provider.name] = getProvider(ctor.name);
-        });
-        return ctor;
+export function Inject(provider: Singleton<Service>): Decorator<Service> {
+    return function(target, key) {
+        target[key] = provider.self;
     }
 }
+
+// class OtherService {
+//     private static readonly _self = new OtherService();
+//     private constructor() {}
+//     static get self() {
+//         return OtherService._self;
+//     }
+//     method() {
+//         setTimeout(() => console.log(`Service provided...`), 1000);
+//     }
+// }
+
+// class A {
+//     @Inject(OtherService) private serve: OtherService;
+//     constructor() { }
+//     method() {
+//         console.log(`Getting service...`);
+//         this.serve.method();
+//     }
+// }
+
+// const a = new A();
+// a.method();
