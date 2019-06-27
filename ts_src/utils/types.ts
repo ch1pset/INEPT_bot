a913104496da53b452a94aaa86f00caddfef88f8
ch1pset <theslenderpsycho@gmail.com>
2019-06-27 00:52:19 -0500

    added type aliases

--- ts_src/utils/types.ts
+++ ts_src/utils/types.ts
@@ -1,4 +1,28 @@
-
-export type fn = Function;
+/**
+ * Primitives
+ */
 export type bool = boolean;
+export type num = number;
+export type str = string;
+
+/**
+ * Functions
+ */
+export type fn = Function;
+export type nodefn = (args: any[], callback: (error: Error, success: any) => void) => any;
+export type Constructor = {new(...args: any[])};
 export type Decorator<T> = (target: T, key?: PropertyKey, method?: PropertyDescriptor) => any;
+export type Singleton<T> = SingletonFunction<T>;
+
+interface SingletonFunction<T> {
+    self: T;
+    apply(this: Function, thisArg: any, argArry?: any): any;
+    call(this: Function, thisArg: any, argArry?: any): any;
+    bind(this: Function, thisArg: any, argArry?: any): any;
+    toString(): string;
+    prototype: any;
+    name: string;
+    readonly length: number;
+    arguments: any;
+    caller: Function;
+};
