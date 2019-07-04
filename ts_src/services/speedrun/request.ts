64d302e00dff760bc5cfa741c9bdd469d0b8fa3b
ch1pset <theslenderpsycho@gmail.com>
2019-07-04 16:47:31 -0500

    fixed ILevel and IGame id types should be strings

--- ts_src/services/speedrun/request.ts
+++ ts_src/services/speedrun/request.ts
@@ -1,20 +1,61 @@
 import { IRequest } from "./interfaces";
-import { OutgoingHttpHeaders } from "http";
+import { str } from "../../utils/typedefs";
 
-export class Request implements IRequest {
-    hostname: string;
-    path: string;
-    headers: OutgoingHttpHeaders;
+export class SrRequest implements IRequest {
+    hostname = "www.speedrun.com";
+    path = '/api/v1';
+    headers = {
+        "Content-Type":"application/json",
+        "User-Agent":"INEPT_bot/0.1.a"
+    };
     method?: string;
-    static create(path: string, method?: string): IRequest {
-        return {
-            hostname: "www.speedrun.com",
-            path: `/api/v1${path? path : ''}`,
-            headers: {
-                "Content-Type":"application/json",
-                "User-Agent":"INEPT_bot/0.1.a"
-            },
-            method
-        }
+    private constructor() { }
+
+    static create(method: str, path: str): IRequest {
+        const req = new SrRequest();
+        req.path += path? path : '';
+        req.method = method ? method.toUpperCase() : 'GET';
+        return req
     }
+}
+
+export class Route {
+    private path: string;
+    constructor(path: string) {
+        this.path = path;
+    }
+    str() {
+        return this.path;
+    }
+    setId(idType: string, idVal: string) {
+        this.path = this.path.replace(':' + idType, idVal);
+        return this;
+    }
+    setIds(...params: string[][]) {
+        for(let [t, v] of params) {
+            this.path = this.path.replace(':' + t, v);
+        }
+        return this;
+    }
+}
+
+export const SRCAPI = {
+    NOTIFY:     new Route('/notifications'),
+    ALL_USERS:  new Route('/users'),
+    ALL_GAMES:  new Route('/games'),
+    ALL_RUNS:   new Route('/runs'),
+    ALL_CATS:   new Route('/categories'),
+    ALL_SERIES: new Route('/series'),
+    USER:       new Route('/users/:uid'),
+    RUN:        new Route('/runs/:rid'),
+    VARIABLE:   new Route('/variables/:vid'),
+    LEVEL:      new Route('/levels/:lid'),
+    CATEGORY:   new Route('/categories/:cid'),
+    GAME:       new Route('/games/:gid'),
+    SERIES:     new Route('/series/:sid'),
+    CAT_VARS:   new Route('/categories/:cid/variables'),
+    LVL_VARS:   new Route('/levels/:lid/variables'),
+    GAME_VARS:  new Route('/games/:gid/variables'),
+    CAT_LEADER: new Route('/leaderboards/:gid/category/:cid'),
+    LVL_LEADER: new Route('/leaderboards/:gid/level/:lid/:cid'),
 }
