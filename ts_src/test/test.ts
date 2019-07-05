db27a41cb358981000cf9f973f7b06710d0b64cb
ch1pset <theslenderpsycho@gmail.com>
2019-07-05 01:33:06 -0500

    added test directory to gitignore

--- ts_src/test/test.ts
+++
@@ -1,4 +0,0 @@
-// import { str } from "../utils/typedefs";
-// import { RespondService } from "../bot/services/respond.service";
-// // import * as Discord from 'discord.js';
-// // import { ServiceA, Service } from "../utils/injector";
