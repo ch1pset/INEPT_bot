ebefc6b65b7ce2e33b03b68a3e1a6099e8b9c141
ch1pset <theslenderpsycho@gmail.com>
2019-07-07 22:49:17 -0500

    Fixed errors stemming from dictionary

--- ts_src/utils/dictionary.ts
+++ ts_src/utils/dictionary.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
-import { Callback, NodeCallback } from './typedefs';
+import { Callback, NodeCallback, bool } from './typedefs';
 import { Subscribable } from './subscriber';
 import { Mixin } from './decorators';
 import { AsyncStat, Stat } from './asyncstat';
+import { Logger } from '../services';
 
 @Mixin([Subscribable, AsyncStat])
 export class Dictionary<T> implements Subscribable, AsyncStat {
@@ -22,82 +23,80 @@ export class Dictionary<T> implements Subscribable, AsyncStat {
     dispatch: (event: string, ...args: any[]) => Subscribable;
     consume: (event: string, ...args: any[]) => Subscribable;
 
-    constructor(fname: string) {
-        this.loadFromFile(fname, (err, dict) => {
-
-        })
-    }
-
-    get array() {
+    get array(): T[] {
         const arr = [];
         for(let key in this) {
             arr.push(this[key]);
         }
         return arr;
     }
-    get size() {
-        let count = 0;
-        for(let k in this) count++;
-        return count;
+    get size(): number {
+        return Object.keys(this).length;
     }
-    set(key: string, value: T) {
+    set(key: string, value: T): void {
         this[key.toLowerCase()] = value;
     }
-    get(key: string) {
+    get(key: string): T {
         if(this.has(key)) {
             const value = <T>{};
             Object.assign(value, this[key.toLowerCase()]);
             return value;
         } else return null;
     }
-    has(key: string) {
-        return this[key.toLowerCase()] ? true : false;
+    has(key: string): bool {
+        return this[key.toLowerCase()] || false;
     }
-    delete(key: string) {
+    delete(key: string): bool {
         this[key.toLowerCase()] = undefined;
-        delete this[key.toLowerCase()];
+        return delete this[key.toLowerCase()];
     }
-    forEach(cb: Callback<void>) {
+    forEach(cb: Callback<void>): void {
         for(let key in this) {
             cb(this.get(key), key);
         }
     }
-    find(cb: Callback<boolean>) {
+    find(cb: Callback<boolean>): T {
         for(let key in this) {
             if(cb(this.get(key), key))
                 return this.get(key);
         }
         return null;
     }
-    writeToFile(fname: string, cb: NodeCallback<Error, Dictionary<T>>) {
-        console.log(`Writing to ${fname}`);
+    writeToFile(fname: string, cb: NodeCallback<Error, Dictionary<T>>): void {
+        Logger.default.info(`Writing to ${fname}`);
         this.busy();
         fs.writeFile(
             fname,
-            JSON.stringify(this, null, 4),
+            JSON.stringify(this, (key, val)=> key === 'status' ? undefined : val, 4),
             err => {
                 if(!err) {
-                    this.consume('done', this.ready());
+                    // this.consume('done', this.ready());
+                    this.ready();
                     cb(null, this);
                 } else {
-                    this.consume('done', this.error());
+                    // this.consume('error', this.error());
+                    this.error();
                     cb(err, null);
                 }
             });
     }
-    loadFromFile(fname: string, cb: NodeCallback<Error, Dictionary<T>>) {
-        console.log(`Reading from ${fname}`);
+    loadFromFile(fname: string, cb: NodeCallback<Error, Dictionary<T>>): void {
+        Logger.default.info(`Reading from ${fname}`);
         this.busy();
         fs.readFile(
             fname,
             'utf8',
             (err, data) => {
-            if(!err) {
-                const temp = JSON.parse(data);
-                Object.assign(this, temp);
-                this.consume('done', this.ready());
-                cb(null, this);
-            } 
-            else cb(err, null)});
+                if(!err) {
+                    const temp = JSON.parse(data);
+                    Object.assign(this, temp);
+                    this.ready();
+                    // this.consume('loaded', this.ready(), this);
+                    cb(null, this);
+                } else {
+                    // this.consume('error', this.error(), err);
+                    this.error();
+                    cb(err, null);
+                }});
     }
 }
