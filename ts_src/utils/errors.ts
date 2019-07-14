aab8f8250797d2674f29156ec5ad6a88f076cd5a
ch1pset <theslenderpsycho@gmail.com>
2019-07-14 15:08:44 -0500

    Removed unused error

--- ts_src/utils/errors.ts
+++ ts_src/utils/errors.ts
@@ -6,12 +6,6 @@ export class ResourceNotFoundError extends Error {
     }
 }
 
-export class OutOfScopeError extends Error {
-    constructor(target: fn, key: PropertyKey) {
-        super(`The property ${String(key)} is out of the scope of ${target.name}.`);
-    }
-}
-
 export class SingletonInstantiationError extends Error {
     constructor(ctor: fn) {
         super(`At 'new ${ctor.name}()' Cannot instantiate Singletons! You can only call singleton instances with the '.self' static property!`)
