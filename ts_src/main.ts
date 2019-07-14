0ad02b79d655a84eeeb998524f26a1c7b8883717
ch1pset <theslenderpsycho@gmail.com>
2019-07-14 15:13:22 -0500

    ILink is now Link class

--- ts_src/main.ts
+++ ts_src/main.ts
@@ -1,8 +1,11 @@
 import * as auth from '../auth.json';
 import { BotClient } from './bot/botclient';
-import { Ping, Speedrun, Links, ILink } from './bot/modules';
+import { Ping, Speedrun, Links, Link } from './bot/modules';
 import * as Service from './services';
 import { PERMISSIONS, UserArgs } from './discord';
+import { Command } from './utils';
+import { Message } from 'discord.js';
+
 
 const bot = new BotClient(
     auth.token.bot,
@@ -13,7 +16,7 @@ const bot = new BotClient(
 bot.subscribe('login', (chLogger: Service.Logger) => {
     
     const dbTasker = new Service.Tasker(chLogger);
-    const linksDB = new Service.DbManager<ILink>(dbTasker, chLogger);
+    const linksDB = new Service.DbManager<Link>(dbTasker, chLogger);
     linksDB.load('./links.json');
 
     const pinger = new Ping(
@@ -35,13 +38,13 @@ bot.subscribe('login', (chLogger: Service.Logger) => {
     links.roles = ['Mods', 'Runners', 'Community-Dev', 'Dev', 'Testers'];
     links.permissions = PERMISSIONS.ADMINISTRATOR | PERMISSIONS.BAN_MEMBERS | PERMISSIONS.KICK_MEMBERS;
 
-    const commands = [
+    const commands: Command[] = [
         ['ping',        (args, msg) => pinger.ping(args, msg)   ],
         ['addlink',     (args, msg) => links.add(args, msg)     ],
         ['deletelink',  (args, msg) => links.delete(args, msg)  ],
         ['getlink',     (args, msg) => links.get(args, msg)     ],
         ['wr',          (args, msg) => speedrun.wr(args, msg)   ],
-        ['commands',    (args, msg) => msg.reply('You can use these commands:\n' + commands.map(cmd => '!' + cmd[0]).join(', '))]
+        ['commands',    (args, msg) => msg.reply('You can use these commands:\n' + commands.map(([name, cb]) => '!' + name).join(', '))]
     ];
     bot.subscribeAll(commands);
 });
--- ts_src/utils/typedefs.ts
+++ ts_src/utils/typedefs.ts
@@ -1,3 +1,6 @@
+import { UserArgs } from "../discord";
+import { Message } from "discord.js";
+
 /**
  * Primitives
  */
@@ -12,6 +15,14 @@ export type arr<T> = Array<T>;
 export type NodeCallback<E, S> = (error: E, success?: S) => void;
 export type Callback<T> = (...args: any[]) => T;
 
+/**
+ * Aliases
+ */
 export type fn = Function;
-export type Constructor = {new(...args: any[])};
+export type Constructor = {new(...args: any[]):{}};
 export type Decorator<T> = (target: T, key: PropertyKey, method?: PropertyDescriptor) => any;
+
+/**
+ * Custom types
+ */
+export type Command = [string, (args: UserArgs, msg: Message) => void];
