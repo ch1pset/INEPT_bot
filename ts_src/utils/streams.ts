754b849248d3c10fe81c03790f60edf90d0a3cfc
ch1pset <theslenderpsycho@gmail.com>
2019-07-14 22:59:55 -0500

    Removed unused imports from streams

--- ts_src/utils/streams.ts
+++ ts_src/utils/streams.ts
@@ -1,7 +1,6 @@
-import { NodeStringDecoder, StringDecoder } from "string_decoder";
 import { Writable, Duplex } from "stream";
-import { Decorator, NodeCallback, Callback } from "./typedefs";
-import { TextChannel, DMChannel, TextBasedChannel } from "discord.js";
+import { Callback } from "./typedefs";
+import { TextChannel } from "discord.js";
 
 export class StringStream extends Duplex {
     private _data: string = '';
