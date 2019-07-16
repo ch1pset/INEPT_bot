ae08b3dbbaeb282b7bd65a8396c2d715e489dc65
ch1pset <theslenderpsycho@gmail.com>
2019-07-16 02:02:56 -0500

    Begin work on gsheets-service expansion

--- ts_src/services/sheets.service.ts
+++ ts_src/services/sheets.service.ts
@@ -1,36 +1,76 @@
 import { sheets } from '../../config.json';
 import * as https from 'https';
 import { GRequest } from './sheets/api'
-import { StringStream, NodeCallback } from '../utils';
-import { Link } from '../bot/modules';
+import { StringStream, NodeCallback, AsyncStatus, Status, Callback, Mixin } from '../utils';
 
-export class Sheets {
-    static getSheet([name, range]: [string, string], cb?: NodeCallback<Error, string[][]>) {
-        const options = new GRequest(sheets[name], range);
+type guide = [string, string, string, string, string, string];
+
+@Mixin([AsyncStatus])
+export class Sheet<T> implements AsyncStatus {
+    eventNames: () => (string | symbol)[];
+    on:     (event: Status | "ready" | "busy" | "error" | "null", listener: Callback<void>) => this;
+    once:   (event: Status | "ready" | "busy" | "error" | "null", listener: Callback<void>) => this;
+    off:    (event: Status | "ready" | "busy" | "error" | "null", listener: Callback<void>) => this;
+    emit:   (event: Status | "ready" | "busy" | "error" | "null", ...args: any[]) => boolean;
+    status:  Status;
+    ready: () => Status;
+    busy: () => Status;
+    error: (err: Error) => Status;
+    isReady: boolean;
+    isBusy: boolean;
+    failed: boolean;
+
+    private _data: T[][];
+
+    constructor(
+        private id: string,
+        private range: string
+    ) { }
+
+    get(cb?: NodeCallback<Error, T[][]>) {
+        this.busy();
+        const options = new GRequest(this.id, this.range);
         const sstream = new StringStream();
         const req = https.request(
             options,
             res => res.pipe(sstream)
                 .on('finish', () => {
                     if(res.statusCode === 200) {
+                        this.ready();
                         cb(null, JSON.parse(sstream.data).values)
-                    } else cb(new Error(`Sheet not found!`));
+                    } else {
+                        const err = new Error(`Sheet not found!`);
+                        this.error(err);
+                        cb(err);
+                    }
                 })
-                .on('error', e => cb(e)));
+                .on('error', e => {
+                    this.error(e);
+                    cb(e)
+                }));
         req.end();
+        return this;
+    }
+    
+    search(cb: (value: T[]) => T[][]) {
+        this.once('ready', () => {
+            const results = this._data.filter(row => {
+                return cb(row)
+            })
+        })
     }
 }
 
-Sheets.getSheet(
-    ['guides', 'A:F'],
-    (err, values) => {
-        if(!err) {
-            console.log(values.slice(1));
-            values.slice(1).forEach(([name, url, op, date, diff, cat]) => {
-                const link = new Link(name, url, op, date);
-            })
-        } else {
-            console.log(err);
-        }
-    }
-)
+// const Guides = new Sheet<guide>(sheets.guides, 'A:F');
+// Guides.get(
+//     (err, values) => {
+//         if(!err) {
+//             console.log(values.slice(1));
+//             values.slice(1).forEach(([name, url, op, date, diff, cat]) => {
+
+//             })
+//         } else {
+//             console.log(err);
+//         }
+//     }
+// )
