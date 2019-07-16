23730d24f9d7b4e8bf6fae29155c327b10c3f18c
ch1pset <theslenderpsycho@gmail.com>
2019-07-16 00:51:31 -0500

    Renamed Restricted to AccessRestrictions

--- ts_src/bot/modules/restricted.module.ts
+++ ts_src/bot/modules/restricted.module.ts
@@ -1,14 +1,19 @@
 import { Permissions } from 'discord.js';
 import { str } from "../../utils";
 import { UserData, ChannelData } from "../../discord";
-const PERMISSION = Permissions.FLAGS;
 
-export class Restricted {
+export class AccessRestrictions {
     roles: str[];
     permissions: number;
     channels: str[];
     chtypes: str[];
-    grantAccess(user: UserData) {
+    constructor(roles: string[], permissions: number, channels?: string[], chtypes?: string[]) {
+        this.roles = roles;
+        this.permissions = permissions;
+        this.channels = channels;
+        this.chtypes = chtypes;
+    }
+    grant(user: UserData) {
         return this.roles.some(r => user.hasRole(r))
             || user.hasPermission(this.permissions);
     }
