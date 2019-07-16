ca0b49cf81f74b2a7e6402892f7f3b9f2b97098d
Joshua Gonzales <jxgonzales@wbhq.com>
2019-07-16 13:13:48 -0500

    #12 Begin refactoring of SrCom service

--- ts_src/services/speedruncom.service.ts
+++ ts_src/services/speedruncom.service.ts
@@ -21,49 +21,64 @@ export class SpeedrunCom implements AsyncStatus {
     isBusy: boolean;
     failed: boolean;
 
-    private sendRequest(options: IRequest, cb: NodeCallback<Error, any>) {
-        const sstream = new StringStream();
-        const req = https.request(
-            options, 
-            res => res.pipe(sstream)
-                .on('finish', () => {
-                if(res.statusCode === 200) {
-                    cb(null, sstream.obj.data);
-                } else {
-                    cb(new Error(res.statusMessage), null);
-                }})
-                .on('error', e => cb(e, null)));
-        req.end();
+    private _game: Game;
+    constructor(
+        private gid: string
+    ) {
+        this.init();
     }
 
-    private getGame(abrv: str, embeds: str[], cb: NodeCallback<Error, Game>) {
-        const query = new URLSearchParams();
-        query.set('abbreviation', abrv);
-        query.set('embed', embeds.join(','));
-        this.sendRequest(
-            new SrRequest('GET', SRCAPI.ALL_GAMES.str() + '?' + query.toString()),
-            (err, resource) => cb(err, new Game(resource[0])));
+    private init() {
+        const query = new URLSearchParams([
+            ['embeds','levels.variables,categories.variables']
+        ]);
+        const options = new SrRequest('GET', SRCAPI.GAME.setId('gid', this.gid), query);
+        
     }
 
-    public getWR(abrv: str, cat: str, vars: str[][], lvl: str, cb: NodeCallback<Error, any>) {
-        this.getGame(abrv, ['levels.variables', 'categories.variables'], (err, game) => {
-            if(!err) {
-                const category = game.getCategory(cat);
-                const level = game.getLevel(lvl);
-                const query = new URLSearchParams();
-                if(vars) vars.forEach(v => {
-                    let catvars = category.getVariableIDs(v[0], v[1]);
-                    if(catvars) {
-                        query.set(`var-${catvars[0]}`, catvars[1]);
-                    }});
-                query.set('top', '1');
-                const path = level ? 
-                    SRCAPI.LVL_LEADER.setIds(['gid', game.id], ['lid', level.id], ['cid', category.id]).str() 
-                    : SRCAPI.CAT_LEADER.setIds(['gid', game.id], ['cid', category.id]).str();
-                this.sendRequest(
-                    new SrRequest('GET', path + '?' + query.toString()),
-                    (err, resource) => cb(err, resource.runs[0].run));
-            }
-            else cb(err, null)});
-    }
+    // private sendRequest(options: IRequest, cb: NodeCallback<Error, any>) {
+    //     const sstream = new StringStream();
+    //     const req = https.request(
+    //         options, 
+    //         res => res.pipe(sstream)
+    //             .on('finish', () => {
+    //             if(res.statusCode === 200) {
+    //                 cb(null, sstream.obj.data);
+    //             } else {
+    //                 cb(new Error(res.statusMessage), null);
+    //             }})
+    //             .on('error', e => cb(e, null)));
+    //     req.end();
+    // }
+
+    // private getGame(abrv: str, embeds: str[], cb: NodeCallback<Error, Game>) {
+    //     const query = new URLSearchParams();
+    //     query.set('abbreviation', abrv);
+    //     query.set('embed', embeds.join(','));
+    //     this.sendRequest(
+    //         new SrRequest('GET', SRCAPI.ALL_GAMES.str() + '?' + query.toString()),
+    //         (err, resource) => cb(err, new Game(resource[0])));
+    // }
+
+    // public getWR(abrv: str, cat: str, vars: str[][], lvl: str, cb: NodeCallback<Error, any>) {
+    //     this.getGame(abrv, ['levels.variables', 'categories.variables'], (err, game) => {
+    //         if(!err) {
+    //             const category = game.getCategory(cat);
+    //             const level = game.getLevel(lvl);
+    //             const query = new URLSearchParams();
+    //             if(vars) vars.forEach(v => {
+    //                 let catvars = category.getVariableIDs(v[0], v[1]);
+    //                 if(catvars) {
+    //                     query.set(`var-${catvars[0]}`, catvars[1]);
+    //                 }});
+    //             query.set('top', '1');
+    //             const path = level ? 
+    //                 SRCAPI.LVL_LEADER.setIds(['gid', game.id], ['lid', level.id], ['cid', category.id]).str() 
+    //                 : SRCAPI.CAT_LEADER.setIds(['gid', game.id], ['cid', category.id]).str();
+    //             this.sendRequest(
+    //                 new SrRequest('GET', path + '?' + query.toString()),
+    //                 (err, resource) => cb(err, resource.runs[0].run));
+    //         }
+    //         else cb(err, null)});
+    // }
 }
