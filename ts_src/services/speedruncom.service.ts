e6f6672cd4db1009f9a1a6cfed698b9df360b907
ch1pset <theslenderpsycho@gmail.com>
2019-07-17 02:01:12 -0500

    #12 Added test code for srcom service

--- ts_src/services/speedruncom.service.ts
+++ ts_src/services/speedruncom.service.ts
@@ -56,49 +56,15 @@ export class SpeedrunCom implements AsyncStatus {
         return this._game.levels.data.find(lvl => lvl.name === name);
     }
 
-    // private sendRequest(options: IRequest, cb: NodeCallback<Error, any>) {
-    //     const sstream = new StringStream();
-    //     const req = https.request(
-    //         options, 
-    //         res => res.pipe(sstream)
-    //             .on('finish', () => {
-    //             if(res.statusCode === 200) {
-    //                 cb(null, sstream.obj.data);
-    //             } else {
-    //                 cb(new Error(res.statusMessage), null);
-    //             }})
-    //             .on('error', e => cb(e, null)));
-    //     req.end();
-    // }
-
-    // private getGame(abrv: str, embeds: str[], cb: NodeCallback<Error, Game>) {
-    //     const query = new URLSearchParams();
-    //     query.set('abbreviation', abrv);
-    //     query.set('embed', embeds.join(','));
-    //     this.sendRequest(
-    //         new SrRequest('GET', SRCAPI.ALL_GAMES.str() + '?' + query.toString()),
-    //         (err, resource) => cb(err, new Game(resource[0])));
-    // }
-
-    // public getWR(abrv: str, cat: str, vars: str[][], lvl: str, cb: NodeCallback<Error, any>) {
-    //     this.getGame(abrv, ['levels.variables', 'categories.variables'], (err, game) => {
-    //         if(!err) {
-    //             const category = game.getCategory(cat);
-    //             const level = game.getLevel(lvl);
-    //             const query = new URLSearchParams();
-    //             if(vars) vars.forEach(v => {
-    //                 let catvars = category.getVariableIDs(v[0], v[1]);
-    //                 if(catvars) {
-    //                     query.set(`var-${catvars[0]}`, catvars[1]);
-    //                 }});
-    //             query.set('top', '1');
-    //             const path = level ? 
-    //                 SRCAPI.LVL_LEADER.setIds(['gid', game.id], ['lid', level.id], ['cid', category.id]).str() 
-    //                 : SRCAPI.CAT_LEADER.setIds(['gid', game.id], ['cid', category.id]).str();
-    //             this.sendRequest(
-    //                 new SrRequest('GET', path + '?' + query.toString()),
-    //                 (err, resource) => cb(err, resource.runs[0].run));
-    //         }
-    //         else cb(err, null)});
-    // }
+    getLink(rel: string) {
+        return this._game.links.find(link => link.rel === rel);
+    }
 }
+
+const ylsrService = new SpeedrunCom('m1zz5210');
+ylsrService.once('ready', () => {
+    const sstream = new StringStream();
+    https.get(
+        ylsrService.getLink('leaderboard').uri,
+        res => res.pipe(sstream));
+})
