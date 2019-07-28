7590b9d26a0f257a05e4e10964f5ae2167576667
ch1pset <theslenderpsycho@gmail.com>
2019-07-27 22:41:09 -0500

    #12 Refactored Query to createQuery

--- ts_src/utils/rest.ts
+++ ts_src/utils/rest.ts
@@ -1,22 +1,16 @@
 import { RequestOptions, OutgoingHttpHeaders } from "http";
+import { URLSearchParams } from "url";
 
 export interface IRequest extends RequestOptions {
     hostname:       string;
     path:           string;
     headers:        OutgoingHttpHeaders;
     method?:        string;
+    body?:          any;
 }
 
 export type QueryParam = [string, string];
 
-export class Query extends URLSearchParams{
-    private constructor(params?: QueryParam[]) {
-        super(params);
-    }
-    static generate(params: QueryParam[]) {
-        return '?' + (new Query(params)).toString();
-    }
-    static create() {
-        return new Query();
-    }
+export function createQuery(params: QueryParam[]) {
+    return '?' + (new URLSearchParams(params)).toString();
 }
