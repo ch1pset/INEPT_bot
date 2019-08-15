243d670916c762458c72530f12dfd26caeaa9747
ch1pset <theslenderpsycho@gmail.com>
2019-08-14 20:21:45 -0500

    Removed if statement from Mixin decorator

--- ts_src/utils/decorators/mixin.dec.ts
+++ ts_src/utils/decorators/mixin.dec.ts
@@ -2,14 +2,12 @@ import { fn } from '../typedefs';
 
 export function Mixin(mixins: fn[]): fn {
     return function(ctor: fn) {
-        if(mixins) {
-            mixins.forEach(mixin => {
-                console.log(`Applying mixin ${mixin.name} to ${ctor.name}...`);
-                const proto = Object.getOwnPropertyDescriptors(mixin.prototype);
-                for(let name in proto) {
-                    Object.defineProperty(ctor.prototype, name, proto[name]);
-                }
-            });
-        }
+        mixins.forEach(mixin => {
+            console.log(`Applying mixin ${mixin.name} to ${ctor.name}...`);
+            const proto = Object.getOwnPropertyDescriptors(mixin.prototype);
+            for(let name in proto) {
+                Object.defineProperty(ctor.prototype, name, proto[name]);
+            }
+        });
     }
 }
